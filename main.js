54770816ef3ef15c8eef7deae72f09af7b25cd42
OmayrHere <omairaslam22@gmail.com>
2025-12-25 18:26:23 +0500

    2025 design update

--- main.js
+++ main.js
@@ -1,39 +1,28 @@
+const navbar = document.querySelector('.navbar');
+const navbarLinks = document.querySelector('.headerA');
+
 function toggleNavbar() {
-    var links = document.querySelectorAll('.navbar a');
-    links.forEach(function(link) {
-      link.style.display = link.style.display === 'block' ? 'none' : 'block';
-    });
-  }
+    navbarLinks.classList.toggle('active');
+}
 
-  function closeNavbar() {
-    if (window.innerWidth <= 600) {
-      var links = document.querySelectorAll('.navbar a');
-      links.forEach(function(link) {
-        link.style.display = 'none';
-      });
+function closeNavbar() {
+    if (window.innerWidth <= 991) {
+        navbarLinks.classList.remove('active');
     }
-  }
+}
 
-  window.addEventListener('resize', closeNavbar);
-
-  window.addEventListener('scroll',function(){
-    var navview = document.querySelector('.navbar');
-    var navheight = navview.offsetHeight;
-
-    if(window.scrollY > navheight){
-                navview.classList.add('sticky');
-   
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 50) {
+        navbar.classList.add('sticky');
+    } else {
+        navbar.classList.remove('sticky');
     }
-    else{
-        navview.classList.remove('sticky');
-    }
+});
 
-  });
-
-  const preloader = document.querySelector('#preloader');
-  if (preloader) {
+const preloader = document.querySelector('#preloader');
+if (preloader) {
     window.addEventListener('load', () => {
-      preloader.remove();
+        preloader.remove();
     });
-  }
+}
 
